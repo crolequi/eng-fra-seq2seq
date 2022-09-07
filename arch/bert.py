c1e897ed421bd4b8fdf6994b6452850e6d96d586
sonvier <segeral@163.com>
2022-09-07 09:26:01 +0800

    Add files via upload

--- arch/bert.py
+++ arch/bert.py
@@ -1,2 +1,30 @@
 import torch
-from arch.transformer import TransformerEncoder
+import torch.nn as nn
+from arch.transformer import TransformerEncoder, TransformerEncoderLayer
+
+
+class BERTEncoder(nn.Module):
+
+    def __init__(self, vocab_size, d_model, nhead, dim_feedforward, dropout, num_layers, max_len=1000):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size, d_model)
+        self.segment_embedding = nn.Embedding(2, d_model)
+        self.position_embedding = nn.Parameter(torch.empty(max_len, 1, d_model))  # (L, N, d)
+        self.encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
+        self.encoder = TransformerEncoder(self.encoder_layer, num_layers)
+
+
+
+        self._reset_parameters()
+
+    def forward(self,):
+        pass
+        
+        
+
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
