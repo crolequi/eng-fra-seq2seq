985b99bb9f583e383f6bb437dee837538edb587d
sonvier <segeral@163.com>
2022-09-09 16:17:04 +0800

    Add files via upload

---
+++ model/bert_seq2seq.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+class BertEmbedding(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    
+    def forward(self, ):
+        pass
+
+
+
+
+class BertModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def forward(self, ):
+        pass
+
+
+
